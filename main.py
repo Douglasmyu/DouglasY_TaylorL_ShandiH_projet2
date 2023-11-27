7e919ee2ca17d7507faab5766c6c95b9fa1a0629
Deku015 <123529985+Deku015@users.noreply.github.com>
2023-11-27 13:37:37 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+f = open("input.txt",'r')
+import dynamic.py
+import exhaustive.py
 
+for i in range(0,10):
+  n = f.readline()
+  s_and_v = f.readline()
+  amount = f.readline()
+  f.readline()
+  max_amount_exhaustive = exhaustive(n, s_and_v, amount)
+  max_amount_dynamic = dynamic(n, s_and_v, amount)
+  
+f.close()
